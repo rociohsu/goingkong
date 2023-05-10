e3b519747892120e8615f870e5b5560ea92a82c1
Rocio Hsu <shachien@gmail.com>
2023-05-10 17:41:38 +0800

    230510 update
    新增footer+主視覺區

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,21 @@
 $(function() {
     /* mobile open menu */
     $('.menu .bar-mobile').click(function(){
-        $('.menu-r').fadeIn(300);
+        $('.menu-r').fadeToggle(300);
     });
 
     /* mobile close menu */
-    $('.menu .menu-r-close').click(function(){
-        $('.menu-r').fadeOut(100);
+    $('.menu .menu-r-close, .container, footer').click(function(){
+        $('.menu-r').fadeOut(300);
+    });
+
+    /* open nav-second */
+    $('.nav-second-open').click(function(){
+        $(this).find('.nav-second').slideToggle(300);
+    });
+
+    /* close nav-second */
+    $('.container, footer').click(function(){
+        $('.nav-second').slideUp(300);
     });
 });
